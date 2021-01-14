82c7a30d3fce1f52d0fff66f89bb21cbba1ba049
Jheam Storch Ross <58143641+srjheam@users.noreply.github.com>
2021-01-13 21:06:41 -0300

    Update app to always get a new wallpaper

--- Wallddit.Core/Reddit/WallpaperProvider.cs
+++ Wallddit.Core/Reddit/WallpaperProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Windows.UI.Xaml.Media.Imaging;
 
@@ -10,38 +11,64 @@ namespace Wallddit.Core.Reddit
     public class WallpaperProvider
     {
         private readonly APIWrapper _apiWrapper;
+        private readonly int _wallpapersPerCall;
+        private readonly Queue<Wallpaper> _wallpapersHistory;
 
         public string SubredditSource { get; }
 
         public WallpaperProvider()
         {
             _apiWrapper = new APIWrapper();
-            SubredditSource = "walpaper"; ;
+            _wallpapersPerCall = 100;
+            _wallpapersHistory = new Queue<Wallpaper>();
+
+            SubredditSource = "walpaper";
         }
 
         public async Task<Wallpaper> GetFreshWallpaperAsync()
         {
-            var apiResponse = await _apiWrapper.GetHotPostsAsync("wallpaper", new Dictionary<string, string>
+            var callParameters = new Dictionary<string, string>
             {
-                ["limit"] = "1"
-            });
-            dynamic redditLink = apiResponse.data.children[0].data;
+                ["limit"] = _wallpapersPerCall.ToString()
+            };
 
+            dynamic redditLink = null;
+            do
+            {
+                var apiResponse = await _apiWrapper.GetHotPostsAsync("wallpaper", callParameters);
+                foreach (var link in apiResponse.data.children)
+                {
+                    if (!_wallpapersHistory.Select(x => x.Id).Contains((string)link.data.name))
+                    {
+                        redditLink = link.data;
+                    }
+                }
+                callParameters.Add("after", (string)apiResponse.data.after);
+            } while (redditLink == null);
+
+            var wallpaper = GetWallpaperFromJson(redditLink);
+            _wallpapersHistory.Enqueue(wallpaper);
+
+            return wallpaper;
+        }
+
+        private Wallpaper GetWallpaperFromJson(dynamic json)
+        {
             Uri wallpaperUri;
             try
             {
-                wallpaperUri = GetWallpaperUriFromGallery(redditLink, 0);
+                wallpaperUri = GetWallpaperUriFromGallery(json, 0);
             }
             catch (Exception)
             {
-                wallpaperUri = new Uri((string)redditLink.url);
+                wallpaperUri = new Uri((string)json.url);
             }
 
-            var wallpaperId = (string)redditLink.name;
-            var wallpaperAuthor = (string)redditLink.author;
+            var wallpaperId = (string)json.name;
+            var wallpaperAuthor = (string)json.author;
             var wallpaperImage = new BitmapImage(wallpaperUri);
-            var wallpaper = new Wallpaper(wallpaperId, wallpaperAuthor, wallpaperImage);
 
+            var wallpaper = new Wallpaper(wallpaperId, wallpaperAuthor, wallpaperImage);
             return wallpaper;
         }
 
